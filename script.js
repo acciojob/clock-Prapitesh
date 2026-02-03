017bf1b6aa70dc4c86d822fa5a7a9322e763939d
Prapitesh <116258736+Prapitesh@users.noreply.github.com>
2026-02-03 12:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+
+function updateTime() {
+  const now = new Date();
+  timer.textContent = now.toLocaleString();
+}
+
+// run immediately
+updateTime();
+
+// update every second
+setInterval(updateTime, 1000);
